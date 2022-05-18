9480cf8a7b698b6702cf88eaae4a9ab231fec0c3
LalaYun <a47636465@gmail.com>
2022-05-18 13:20:12 +0900

    hbd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return '../public/index.html';
+  // return (
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //   <img alt="Yuntted logo" src="./logo.png" width="200px" height="200px"/>
+    //   <p style="font-family: 'IBM Plex Sans KR', sans-serif;">윤티드 도넛은 언제나 이벤트가 팡팡!<br/>
+    //     당신이 원하는 것을 얻게 됩니다.</p><br/>
+    //   <img alt="Enter code" src="./code1.png" width="200px" height="50px"/><br/>
+    //   <input id="check_input"
+    //     type="text"
+    //     placeholder="here" style="margin: 5px; font-size:17px; width:200px; height:30px; border:#FFF0F0 1px solid; font-family: 'IBM Plex Sans KR', sans-serif;"
+    //     /><br/>
+
+    //   <button id="check_button" class="btn" type="button" 
+    //   style="font-size:17px; font-color: #483D8B; font-family: 'IBM Plex Sans KR', sans-serif ;width:80px; height: 30px; border: 1px #483D8B solid; border-radius: 2px; background-color: #FFFFFF;">click</button>
+    //   <div id="result_div" class="text-center mt-2"></div>
+    //   <p style="font-family: 'IBM Plex Sans KR', sans-serif;"></p>
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //   </header>
+    // </div>
+  // );
 }
 
 export default App;
